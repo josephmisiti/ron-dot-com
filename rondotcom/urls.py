5a77715fcaa0f6bb39d9485617939f00c79a40bb
joseph misiti <joseph.misiti@gmail.com>
2013-04-15 18:20:50 -0400

    first commit

---
+++ rondotcom/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', 	'apps.static.views.index', name='index'),
+)
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
+    )
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.STATIC_ROOT}),
+    )
